71eecacaaa66ae46cbb742177460514af8e82cdb
Abishek M <167514119+Abishek25112004@users.noreply.github.com>
2026-05-09 12:50:49 +0530

    cfp

---
+++ server/script.js
@@ -0,0 +1,1 @@
+const { PrismaClient } = require('@prisma/client'); const prisma = new PrismaClient(); prisma.siteContent.findUnique({where: {key: 'call_for_papers'}}).then(console.log).finally(() => prisma.$disconnect());
